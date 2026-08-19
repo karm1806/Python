58c8f7a33e5618e20eb006608f540f010305aec3
karm1806 <niravac@gmail.com>
2026-08-19 19:12:42 +0530

    commit

---
+++ Natural Number/NN.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a Number: "))
+if(number>0):
+    print("It is a Positive Number")
+elif(number<0):
+    print("It is a Negative Number")
+else:
+    print("It is a Neutral Number")
