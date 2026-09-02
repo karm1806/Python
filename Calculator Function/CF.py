f965280b8fd3748d56ffe5c207f0a4032c4cfb6d
karm1806 <niravac@gmail.com>
2026-09-02 19:26:25 +0530

    commit

--- Calculator Function/CF.py
+++ Calculator Function/CF.py
@@ -1,7 +1,14 @@
 print("Calculator")
 num=(int(input("Enter a Number : ")))
-add=(int(input("Enter Another Number to Add :")))
-num = sum
-def addsum():
-    sum=num+add
-print(number)
+add=(int(input("Enter Another Number : ")))
+sym=input("Enter the Symbol to calculate : ")
+if sym == "+":
+    print(num+add)
+elif sym == "-":
+    print(num-add)
+elif sym == "*":
+    print(num*add)
+elif sym == "/":
+    print(num/add)
+else:
+    print ("Enter a symbol from +, -, *, / ")
