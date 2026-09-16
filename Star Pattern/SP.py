04f12f36141bfc80899135367ceb3691d3b0fd77
karm1806 <niravac@gmail.com>
2026-09-16 20:37:12 +0530

    commit

--- Star Pattern/SP.py
+++ Star Pattern/SP.py
@@ -3,3 +3,8 @@ for i in range (1,6):
     for j in range(i):
         print("*", end="")
     print('\n')
+print("Inverted Star pattern \n")
+for i in range(6,1,-1):
+    for j in range(i,1,-1):
+        print("*", end="")
+    print('\n')
